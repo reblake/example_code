32e683478c587db6c3ff6f3fb3224219e6ebd8b7
Rachael Blake <rblake@rstudio03.research.sesync.org>
2021-03-30 08:59:32 -0400

    add coleoptera/lepidoptera taxonomy script

---
+++ make_tax_table_Col_Lep.R
@@ -0,0 +1,23 @@
+####################################################################
+##### Insect Invasions Pursuit @ SESYNC                        #####
+##### Script to update Coleoptera and Lepidoptera families     #####
+##### created by Rachael Blake    03/23/2021                   #####
+####################################################################
+
+# load packages
+library(tidyverse)
+
+# load taxonomy table
+tax_table <- read_csv("nfs_data/data/clean_data/taxonomy_table.csv")
+
+# load Coleoptera fixes
+coleop_fix <- read_csv("")
+
+# load Lepidoptera fixes
+lepi_fix <- read_csv("")
+
+# merge Coleoptera fixes with tax table
+
+
+# merge Lepidoptera fixes with Coleop-tax table
+
