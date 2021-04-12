9e55780ca85f01dac55d3f02be6008e0993c77ba
Rachael Blake <rblake@rstudio03.research.sesync.org>
2021-04-12 16:38:41 -0400

    finished coel-lep taxonomy table; wrote to data directory

--- make_tax_table_Col_Lep.R
+++ make_tax_table_Col_Lep.R
@@ -14,39 +14,31 @@ tax_table <- read_csv("nfs_data/data/clean_data/taxonomy_table.csv")
 coleop_fixes <- read_excel("nfs_data/data/raw_data/taxonomic_reference/Coleoptera_family_synonyms.xlsx")
 
 ### load Lepidoptera fixes
-lepi_fixes <- read_csv("nfs_data/data/raw_data/taxonomic_reference/Lep_genus_family_transfers.csv") 
+lepi_fixes <- read_csv("nfs_data/data/raw_data/taxonomic_reference/Lep_genus_family_transfers.csv") %>% 
+              dplyr::rename(family_lep = family)
 
-### merge Coleoptera fixes with tax table
-tt_c <- tax_table %>% 
-        left_join(coleop_fixes, by = c("family" = "user_supplied_family_name")) %>% 
-        mutate(family = ifelse(!is.na(actual_family), actual_family, family),
-               family = ifelse(family %in% c("Dryophthoridae","Brachyceridae"), "Curculionidae", family)) %>% 
-        select(-actual_family)
+### merge Coleoptera fixes with taxonomy table
+tt_col <- tax_table %>% 
+          left_join(coleop_fixes, by = c("family" = "user_supplied_family_name")) %>% 
+          mutate(family = ifelse(!is.na(actual_family), actual_family, family),
+                 family = ifelse(family %in% c("Dryophthoridae","Brachyceridae"), "Curculionidae", family)) %>% 
+          select(-actual_family)
   
  
- 
-# tt3 <- coleop_fixes
-# temp <- read.csv("nfs_data/data/clean_data/dated_tax_files/Establishment2021April01.csv")
-#     
-# for (i in 1:nrow(coleop_fixes)){
-#   temp[temp$family== coleop_fixes[i,]$user_supplied_family_name &!is.na(temp$family),"family"]<- coleop_fixes [i,]$actual_family
-# }
-# 
-# temp[temp$family%in%c("Dryophthoridae","Brachyceridae"),"family"]<-"Curculionidae"
-
-
-
-### merge Lepidoptera fixes with Coleop-tax table
-
-# requires genus
-temp$genus<-word(temp$genus_species,1)
-
+### merge Lepidoptera fixes with Coleoptera-taxonomy table
+tt_col_lep <- tt_col %>% 
+              mutate(genus = ifelse(is.na(genus), word(genus_species, 1), genus)) %>%  # fill in genus if missing
+              left_join(lepi_fixes, by = "genus") %>% 
+              mutate(family = ifelse(!is.na(family_lep), family_lep, family)) %>% 
+              select(-family_lep) %>% 
+              mutate(family = ifelse(family %in% c("Lymantriidae","Arctiidae"), "Erebidae", family),
+                     family = ifelse(family == "Acrolophidae", "Tineidae", family))
 
 
 #####################################
 ### Write file                    ###
 #####################################
 # write the clean taxonomy table to a CSV file
-readr::write_csv(XXXXX, "nfs_data/data/clean_data/taxonomy_table_coel_lep.csv")
+readr::write_csv(tt_col_lep, "nfs_data/data/clean_data/taxonomy_table_coel_lep.csv")
 
 
