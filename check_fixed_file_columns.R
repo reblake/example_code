446772592d335e6c59926eebc42769c466c70fc8
Rachael Blake <rblake@sesync.org@sesync00.research.sesync.org>
2021-12-16 09:57:37 -0500

    finish checking columns

--- check_fixed_file_columns.R
+++ check_fixed_file_columns.R
@@ -6,7 +6,7 @@
 
 # install.packages("devtools")
 # devtools::install_github("reblake/insectcleanr")
-library(insectcleanr)
+library(insectcleanr) ; library(purrr) ; library(tidyverse)
 
 # load the genus_only_resolution_FIXED file
 fixed <- read.csv("./nfs_data/data/raw_data/taxonomic_reference/genus_only_resolution_FIXED.csv")
@@ -14,16 +14,46 @@ fixed <- read.csv("./nfs_data/data/raw_data/taxonomic_reference/genus_only_resol
 # want to send both the user_supplied_name column and the genus_species column through the 
 # get_accepted_taxonomy() function
 
-# start with the user_supplied_name column
+##### start with the user_supplied_name column
 usn <- fixed %>% 
        select(user_supplied_name) %>% 
        filter(!(user_supplied_name %in% c("Aphidiinae", "Arionidae", "Bostrychidae sinoxylon",
                                           "Corticariidae", "Eriosomatinae", "Lyctinae", 
                                           "Nemobiinae", "Omaliinae", "Pscoptera",
                                           "Psocopetra", "Psocoptera", "Scolytidae",
-                                          "Scolytinae", "Spondylidinae"))) %>%   # these taxa cause problems!!
+                                          "Scolytinae", "Spondylidinae", "Syntominae"))) %>%   # these taxa cause problems!!
        unlist(., use.names = FALSE) 
-       
 
+# run through accepted taxonomy function
 usn_clean <- lapply(usn, get_accepted_taxonomy)
+
+# make into one dataframe
+suppressMessages(
+usn_acc <- usn_clean %>%
+           purrr::reduce(full_join) %>%
+           mutate(genus_species = str_squish(genus_species)) 
+)
+
+# write out file
+write_csv(usn_acc, "User_Supp_Nm_FIXED.csv")
+
+
+##### next do the genus_species column
+gs <- fixed %>% 
+      select(genus_species) %>% 
+      filter(!(genus_species %in% "")) %>% 
+      unlist(., use.names = FALSE)   
+
+# run through accepted taxonomy function
+gs_clean <- lapply(gs, get_accepted_taxonomy)
+
+# make into one dataframe
+suppressMessages(
+gs_acc <- gs_clean %>%
+          purrr::reduce(full_join) %>%
+          mutate(genus_species = str_squish(genus_species)) 
+)
+
+# write out file
+write_csv(gs_acc, "Genus_Sp_FIXED.csv")
 
